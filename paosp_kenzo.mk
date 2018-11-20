e84416cb519d8393109548fdd40afee1f28aae2d
rocknegi <rocknegi53@gmail.com>
2018-11-20 15:12:55 +0000

    pornify
    Change-Id: Ifa33fed79dc389c29ee82c73eb99bb5d686ed550

--- paosp_kenzo.mk
+++ paosp_kenzo.mk
@@ -15,22 +15,26 @@
 # limitations under the License.
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from kenzo device
 $(call inherit-product, device/xiaomi/kenzo/full_kenzo.mk)
+$(call inherit-product, vendor/paosp/config/common_full_phone.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/carbon/config/common.mk)
-
-# Inherit Carbon GSM telephony parts
-$(call inherit-product, vendor/carbon/config/gsm.mk)
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := carbon_kenzo
+PRODUCT_NAME := paosp_kenzo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 3
 TARGET_VENDOR := Xiaomi
+
+PRODUCT_PROPERTY_OVERRIDES += \
+   ro.paosp.maintainer=rocknegi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
